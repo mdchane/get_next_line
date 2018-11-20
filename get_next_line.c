67ae5b58100059665f770f1c29a65d267e627d78
Mohamed DCHANE <mdchane@e2r3p10.42.fr>
2018-11-20 09:47:30 +0100

    ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mdchane <mdchane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 10:32:40 by mdchane           #+#    #+#             */
-/*   Updated: 2018/11/17 12:52:47 by mdchane          ###   ########.fr       */
+/*   Updated: 2018/11/20 09:47:17 by mdchane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,30 +31,42 @@ int     index_end_line(char *str)
     return (-1);
 }
 
+char    *ft_read_it(char *str, int fd)
+{
+    int     nbread;
+    char    buff[BUFF_SIZE + 1];
+
+    while ((nbread = read(fd, (void *)buff, BUFF_SIZE)) > 0)
+    {
+        buff[nbread] = '\0';
+        str = ft_strjoin(str, buff);
+    }
+    return (str);
+}
+
 int    get_next_line(const int fd, char **line)
 {
-    size_t      nbread;
-    char        *buff;
+    
     static char *str;
     int         i;
 
     /*if (fd < 0 || line == NULL || fd < 10240)
         return (-1);*/
-    buff = ft_strnew(BUFF_SIZE);
-    str = ft_strnew(BUFF_SIZE);
-    line = (char **)malloc(sizeof(char *));
-    *line = ft_strnew(BUFF_SIZE);
-    while ((nbread = read(fd, (void *)buff, BUFF_SIZE)) > 0)
+    str = ft_strnew(BUFF_SIZE + 1);
+    *line = ft_strnew(BUFF_SIZE + 1);
+    str = ft_read_it(str, fd);
+    i = 0;
+    if (str[i])
     {
-        buff[nbread] = '\0';
-        str = ft_strjoin(str, buff);
         i = index_end_line(str);
-        if (i > -1)
+        if (i == 0)
+            *line = ft_strdup("");
+        else if (i > 0)
         {
             *line = ft_strsub(str, 0, i + 1);
-            printf("%s\n", *line);
-            return (1);
+            str = str + i + 1;
         }
+        return (1);
     }
     return (0);
 }
@@ -70,11 +82,18 @@ int     main(int argc, char **argv)
     char    *line;
     int     ret;
 
-    (void)argc;
-    line = (char *)malloc(sizeof(char));
-    fd = open(argv[1], O_RDONLY);
-    ret = get_next_line(fd, &line);
-    ret++;
-    close(fd);
+    if (argc == 1)
+		fd = 0;
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+    while (get_next_line(fd, &line) == 1)
+	{
+		ft_putstr(line);
+		free(line);
+	}
+	if (argc == 2)
+		close(fd);
     return (0);
 }
