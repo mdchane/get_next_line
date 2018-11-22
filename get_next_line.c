ccfc9e6160f78d08c2da35a2638ff7f08dbf5ebd
Mohamed DCHANE <mdchane@e3r4p10.42.fr>
2018-11-22 12:33:37 +0100

    all done

--- get_next_line.c
+++ get_next_line.c
@@ -6,84 +6,73 @@
 /*   By: mdchane <mdchane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 10:32:40 by mdchane           #+#    #+#             */
-/*   Updated: 2018/11/20 15:49:11 by mdchane          ###   ########.fr       */
+/*   Updated: 2018/11/22 12:14:00 by mdchane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
 
-char    *ft_read_it(char **str, int fd)
+char	*ft_read_it(char **str, int fd, int *error)
 {
-    int     nbread;
-    char    buff[BUFF_SIZE + 1];
+	int		nbread;
+	char	buff[BUFF_SIZE + 1];
+	char	*tmp;
 
-    while ((nbread = read(fd, (void *)buff, BUFF_SIZE)) > 0)
-    {
-        buff[nbread] = '\0';
-        *str = ft_strjoin(*str, buff);
-        if (ft_strchr(*str, '\n'))
-            return (*str);
-    }
-    return (*str);
-}
-
-int     ft_errors(char **str, int fd, char **line)
-{
-    if (fd < 0 || line == NULL || BUFF_SIZE < 0 || fd > OPEN_MAX)
-        return (1);
-    if (!(*str))
-    {
-        if (!(*str = ft_strnew(BUFF_SIZE)))
-            return (1);
-    }
-    return (0);
-}
-
-int    get_next_line(const int fd, char **line)
-{
-    
-    static char *str;
-    int         i;
-
-    if (ft_errors(&str, fd, line))
-        return (-1);
-    str = ft_read_it(&str, fd);
-    i = 0;
-    if (str[i])
-    {
-        while (str[i] && str[i] != '\n')
-            i++;
-        *line = ft_strsub(str, 0, i);
-        str = &str[i + 1];
-        return (1);
-    }
-    else
-        *line = ft_strdup("");
-    return (0);
-}
-
-int     main(int argc, char **argv)
-{
-    int     fd;
-    char    *line;
-
-    if (argc == 1)
-		fd = 0;
-	else if (argc == 2)
-		fd = open(argv[1], O_RDONLY);
-	else
-		return (2);
-    while (get_next_line(fd, &line) == 1)
+	while ((nbread = read(fd, (void *)buff, BUFF_SIZE)) > 0)
 	{
-		ft_putendl(line);
-		ft_strdel(&line);
+		buff[nbread] = '\0';
+		if (!(tmp = ft_strjoin(*str, buff)))
+		{
+			*error = -1;
+			return (NULL);
+		}
+		ft_strdel(str);
+		*str = tmp;
+		if (ft_strchr(*str, '\n'))
+			return (*str);
 	}
-	if (argc == 2)
-		close(fd);
-    return (0);
+	if (nbread < 0)
+		*error = -1;
+	return (*str);
+}
+
+int		ft_errors(char **str, int fd, char **line)
+{
+	if (fd < 0 || line == NULL || fd > OPEN_MAX)
+		return (1);
+	if (!(*str))
+	{
+		if (!(*str = ft_strnew(0)))
+			return (1);
+	}
+	return (0);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char	*str[OPEN_MAX];
+	int			i;
+	char		*tmp;
+	int			error;
+
+	if (ft_errors(&str[fd], fd, line))
+		return (-1);
+	str[fd] = ft_read_it(&str[fd], fd, &error);
+	if (error < 0)
+		return (-1);
+	i = 0;
+	if (str[fd][i])
+	{
+		while (str[fd][i] && str[fd][i] != '\n')
+			i++;
+		*line = ft_strsub(str[fd], 0, i);
+		tmp = ft_strsub(str[fd], i + 1, ft_strlen(str[fd] + i + 1));
+		if (!(*line) || !tmp)
+			return (-1);
+		ft_strdel(&str[fd]);
+		str[fd] = tmp;
+		return (1);
+	}
+	ft_strdel(&str[fd]);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mdchane <mdchane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 10:27:16 by mdchane           #+#    #+#             */
-/*   Updated: 2018/11/20 09:32:22 by mdchane          ###   ########.fr       */
+/*   Updated: 2018/11/22 09:49:36 by mdchane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,13 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <limits.h>
 
-#define BUFF_SIZE 32
+# define BUFF_SIZE 32
 
 int get_next_line(const int fd, char **line);
 #endif
